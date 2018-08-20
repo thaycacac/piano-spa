d81abbba0cee2111ab916cf4694255259d2a7928
thaycacac <thaycacac@gmail.com>
2018-08-20 22:29:01 +0700

    add interface

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,103 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li class="white"></li>
+      <li class="black"></li>
+      <li class="white"></li>
+      <li class="black"></li>
+      <li class="white"></li>
+      <li class="black"></li>
+      <li class="white special"></li>
+      <li class="white"></li>
+      <li class="black"></li>
+      <li class="white"></li>
+      <li class="black"></li>
+      <li class="white"></li>
+      <li class="white"></li>
+      <li class="black"></li>
+      <li class="white"></li>
+      <li class="black"></li>
+      <li class="white"></li>
+      <li class="black"></li>
+      <li class="white"></li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <audio class="keyA" src="../assets/E4.mp3"></audio>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  mounted(){
+    window.addEventListener('keydown', (e) => {
+      const audio = document.querySelector('.keyA')
+      console.log(audio);
+      if(!audio) return;
+      audio.currentTime = 0
+      audio.play()
+    })
   }
-}
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style scoped lang="scss">
+main {
+  text-align: center;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  list-style: none;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  overflow: hidden;
+  padding-left: 1.75vw;
+  padding-bottom: 1em;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.special {
+  margin-right: 27px;
 }
-a {
-  color: #42b983;
+.white {
+  background: -webkit-linear-gradient(110deg, #333, #000, #333);
+  background: linear-gradient(
+    -20deg,
+    rgb(255, 255, 255),
+    rgb(204, 204, 204),
+    rgb(223, 223, 223)
+  );
+  border: 1px solid #ccc;
+  border-radius: 0 0 3px 3px;
+  box-shadow: inset 0 1px 0 #fff, inset 0 -1px 0 #fff, inset 1px 0 0 #fff,
+    inset -1px 0 0 #fff, 0 4px 3px rgba(0, 0, 0, 0.7);
+  height: 40vh;
+  width: 5vw;
+  margin-left: -2vw;
+}
+.white:nth-child(12n + 6),
+.white:nth-child(12n + 13) {
+  margin: 0;
+}
+.white:active {
+  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.4);
+}
+.black {
+  background: -webkit-linear-gradient(110deg, #333, #000, #333);
+  background: linear-gradient(-20deg, #333, #000, #333);
+  border-color: #666 #222 #111 #555;
+  border-radius: 0 0 2px 2px;
+  border-style: solid;
+  border-width: 1px 2px 7px;
+  box-shadow: inset 0 -1px 2px hsla(0, 0%, 100%, 0.4),
+    0 2px 3px rgba(0, 0, 0, 0.4);
+  height: 25vh;
+  position: relative;
+  width: 3.5vw;
+  margin-left: -1.75vw;
+  z-index: 1;
+}
+.black:active {
+  border-bottom-width: 2px;
+  box-shadow: inset 0 -1px 1px hsla(0, 0%, 100%, 0.4),
+    0 1px 0 rgba(0, 0, 0, 0.7), 0 2px 2px rgba(0, 0, 0, 0.4), 0 -1px 0 #000;
 }
 </style>
