8c5b645bba1d6d6a82b17363a162723eaa33f630
thaycacac <thaycacac@gmail.com>
2018-08-21 00:17:29 +0700

    add presskey then play

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,97 @@
 <template>
   <div>
     <ul>
-      <li class="white"></li>
-      <li class="black"></li>
-      <li class="white"></li>
-      <li class="black"></li>
-      <li class="white"></li>
-      <li class="black"></li>
-      <li class="white special"></li>
-      <li class="white"></li>
-      <li class="black"></li>
-      <li class="white"></li>
-      <li class="black"></li>
-      <li class="white"></li>
-      <li class="white"></li>
-      <li class="black"></li>
-      <li class="white"></li>
-      <li class="black"></li>
-      <li class="white"></li>
-      <li class="black"></li>
-      <li class="white"></li>
+      <li class="white KeyA"></li>
+      <li class="black KeyW"></li>
+      <li class="white KeyS"></li>
+      <li class="black KeyE"></li>
+      <li class="white KeyD"></li>
+      <li class="black KeyR"></li>
+      <li class="white special KeyF"></li>
+      <li class="white KeyG"></li>
+      <li class="black KeyT"></li>
+      <li class="white KeyH"></li>
+      <li class="black KeyU"></li>
+      <li class="white KeyJ"></li>
+      <li class="white KeyK"></li>
+      <li class="black KeyI"></li>
+      <li class="white KeyL"></li>
+      <li class="black KeyO"></li>
+      <li class="white Semicolon"></li>
+      <li class="black KeyP"></li>
+      <li class="white Quote"></li>
     </ul>
-    <audio id="KeyA">
+    <audio class="KeyA"  preload='none'>
       <source src="../assets/e4.mp3" type="audio/mpeg">
     </audio>
+    <audio class="KeyS">
+      <source src="../assets/g4.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="KeyD">
+      <source src="../assets/a4.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="KeyF">
+      <source src="../assets/b4.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="KeyG"  preload='none'>
+      <source src="../assets/c5.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="KeyH">
+      <source src="../assets/d5.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="KeyJ">
+      <source src="../assets/e5.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="KeyK">
+      <source src="../assets/f5.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="KeyL">
+      <source src="../assets/g5.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="Semicolon">
+      <source src="../assets/a5.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="Quote">
+      <source src="../assets/b5.mp3" type="audio/mpeg">
+    </audio>
+
+    <audio class="KeyW">
+      <source src="../assets/f4g4.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="KeyE">
+      <source src="../assets/g4a4.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="KeyR">
+      <source src="../assets/a4b4.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="KeyT">
+      <source src="../assets/c5d5.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="KeyU">
+      <source src="../assets/d5e5.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="KeyI">
+      <source src="../assets/f5g5.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="KeyO">
+      <source src="../assets/g5a5.mp3" type="audio/mpeg">
+    </audio>
+    <audio class="KeyP">
+      <source src="../assets/a5b5.mp3" type="audio/mpeg">
+    </audio>
   </div>
-</template>
+</template> 
 
 <script>
 export default {
   mounted() {
     window.addEventListener("keydown", e => {
-      const audio = document.getElementById(e.code); 
-      audio.play();
+      const audio = document.getElementsByClassName(e.code);
+      audio[1].play();
+      audio[0].classList.add("presskey");
+      setInterval(function() {
+        audio[0].classList.remove("presskey");
+      }, 400);
     });
   }
 };
@@ -54,7 +113,7 @@ ul {
   padding-bottom: 1em;
 }
 .special {
-  margin-right: 27px;
+  margin-right: 20px;
 }
 .white {
   background: -webkit-linear-gradient(110deg, #333, #000, #333);
@@ -79,6 +138,9 @@ ul {
 .white:active {
   box-shadow: 0 2px 2px rgba(0, 0, 0, 0.4);
 }
+.presskey {
+  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.4);
+}
 .black {
   background: -webkit-linear-gradient(110deg, #333, #000, #333);
   background: linear-gradient(-20deg, #333, #000, #333);
@@ -95,6 +157,11 @@ ul {
   z-index: 1;
 }
 .black:active {
+  border-bottom-width: 2px;
+  box-shadow: inset 0 -1px 1px hsla(0, 0%, 100%, 0.4),
+    0 1px 0 rgba(0, 0, 0, 0.7), 0 2px 2px rgba(0, 0, 0, 0.4), 0 -1px 0 #000;
+}
+.press-black {
   border-bottom-width: 2px;
   box-shadow: inset 0 -1px 1px hsla(0, 0%, 100%, 0.4),
     0 1px 0 rgba(0, 0, 0, 0.7), 0 2px 2px rgba(0, 0, 0, 0.4), 0 -1px 0 #000;
